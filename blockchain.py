5505525307f75ac672b8919ae82cc81bad6c7e94
qinl <dreamkong0113@gmail.com>
2018-05-14 17:19:54 +0800

    使用flask实现节点通信

--- blockchain.py
+++ blockchain.py
@@ -74,6 +74,64 @@ class BlockChain:
         return guess_hash[0:4] == '0000'
 
 
+app = Flask(__name__)
+blockChain = BlockChain()
+
+node_identifier = str(uuid4()).replace('-', '')
+
+
+@app.route('/index', methods=['GET'])
+def index():
+    return 'Hello BC'
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transactions():
+    values = request.get_json()
+
+    # 检查POST数据
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockChain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockChain.last_block
+    last_proof = blockChain.last_block['proof']
+    proof = blockChain.proof_of_work(last_proof)
+
+    blockChain.new_transaction(sender="0",
+                               recipient=node_identifier,
+                               amount=1)
+    block = blockChain.new_block(proof, None)
+
+    response = {
+        "message": 'New Block Forged',
+        "index": block['index'],
+        "transactions": block['transactions'],
+        "proof": block['proof'],
+        "previous_hash": block['previous_hash']
+    }
+    return jsonify(response), 201
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockChain.chain,
+        'length': len(blockChain.chain)
+    }
+    return jsonify(response), 201
+
+
 if __name__ == '__main__':
-    testPow = BlockChain()
-    testPow.proof_of_work(100)
+    # testPow = BlockChain()
+    # testPow.proof_of_work(100)
+    app.run(host='0.0.0.0', port=5000, debug=True)
